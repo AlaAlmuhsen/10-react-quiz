4d7ad7251d368f6b9aa94cd230a568dba5842449
Alaa Almuhsen <ala.almuhsen@gmail.com>
2025-05-09 18:10:28 +0300

    finish without timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,120 @@
-import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Error from "./components/Error";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import Main from "./components/Main";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import Footer from "./components/Footer";
+import NextButton from "./components/NextButton";
+import FinishScreen from "./components/FinishScreen";
+import Progress from "./components/Progress";
+import Timer from "./components/Timer";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  secondsRemaining: null,
+};
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions[state.index];
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [{ questions, status, answer, points, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((prev, cur) => {
+    return prev + cur.points;
+  }, 0);
+
+  // load the data on mount
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => {
+        dispatch({ type: "dataFailed" });
+      });
+  }, []);
+
   return (
-    <>
-      <DateCounter />
-    </>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen dispatch={dispatch} />}
+        {status === "active" && (
+          <>
+            <Progress
+              numQuestions={numQuestions}
+              index={index}
+              answer={answer}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer />
+              <NextButton
+                answer={answer}
+                dispatch={dispatch}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
+        )}
+      </Main>
+    </div>
   );
 }
 
